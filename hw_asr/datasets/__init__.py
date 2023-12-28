9ea6286aaa2e16b418a361b5bfd6643de3a6e933
Egor Snigirev <43299958+egorsni@users.noreply.github.com>
2023-12-28 18:02:04 +0300

    Update __init__.py

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -1,16 +1,5 @@
-from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
-from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
-from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
-from hw_asr.datasets.ljspeech_dataset import LJspeechDataset
-from hw_asr.datasets.common_voice import CommonVoiceDataset
-
 from hw_asr.datasets.mixed_librispeech_dataset import MixedLibrispeechDataset
 
 __all__ = [
-    "LibrispeechDataset",
-    "CustomDirAudioDataset",
-    "CustomAudioDataset",
-    "LJspeechDataset",
-    "CommonVoiceDataset",
     'MixedLibrispeechDataset'
 ]
